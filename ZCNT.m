f10e608cb86c188ff522c3d928adf2f57ddb6927
Adit1313 <aditkirtani@gmail.com>
2020-10-26 10:35:30 +0530

    Added ZCNT Program

--- ZCNT.m
+++ ZCNT.m
@@ -1,34 +1,36 @@
 close all;
 clear;
 clc;
-fprintf('\t\t\tARMCHAIR GRAPHENE NANORIBBON (E-K Diagram)\n\n\n');
+fprintf('\t\t\tZigzag Carbon Nanotube (E-K Diagram)\n\n\n');
 
-n = input('Enter number of atoms in each ring of AGNR: ');  % Number of atoms at each ring of AGNR
-N_Ring_Unit = 2;     % Number of rings
+n = input('Input 2 * index of your ZCNT: ');  % Number of atoms on the edge of ZCNT.
+N_Ring_Unit = input('Enter number of rings in ZCNT: ');     % Number of rings in unit cell
 
-N_Total_Plot = (n*N_Ring_Unit*3);    % Number of atoms
+N_Total_Plot = (n*N_Ring_Unit*3);    % Number of atoms in the Plot
 
-%Distances:
-a0 = 1.42;      % Lattice Constant (in Angstrom)
+% Basic Distances in Carbon Honey-Comb Structure
+a0 = 1.42;      % Lattice Constant (in Agnestrom)
 d1=a0;          % First-neighbours distance = a0
-d2=2.45951214;  % Second-neighbours distance = a0*sqrt(3)
-d3=2.84;        % Third-neighbours distance = (2*a0)
-mrg = 0.01;    % Margin
+d2=a0*sqrt(3);  % Second-neighbours distance = a0*sqrt(3)
+d3=2*a0;        % Third-neighbours distance
+mrg = 0.01;    % Margin needed in calculations (small value)
 
 % Hopping Parameters
 eps=0;       % On site potential for Carbon
 t1=-2.65;       % First-neighbour interaction potential
 t2=0;        % Second-neighbour interaction potential
-t3=-0.27;           % Third-neighbour interaction potential
+t3=0;           % Third-neighbour interaction potential
 
-D_w = (n-1)*(a0*sqrt(3)/2);   % Ribbon width
+D_w = ((n-1)*(a0*sqrt(3)/2));
 Unit_L = N_Ring_Unit*1.5*a0;
-fprintf('Ribbon Width = %.2f nm\n\n',D_w/10);
-
+D_r = (D_w + a0*sind(60))/(2*pi);      % Tube Radius of CNT
+fprintf('Device Radius = %.2f nm \n\n',D_r/10);
 fprintf('Generating atoms sites...');
 
+% Assigning Geometric position to each atom
 atom_x=zeros(1,N_Total_Plot);     % X site of atoms
 atom_y=zeros(1,N_Total_Plot);     % Y site of atoms
+
 
 if mod(n,2) == 0
     for j=1:(N_Ring_Unit/2)*3
@@ -58,19 +60,19 @@ else
     end
 end
 
-%Plotting atoms
+% Plotting the atoms
 for i=1:N_Total_Plot
     plot(atom_x(i),atom_y(i),'o','MarkerSize',8,'MarkerEdgeColor','b','MarkerFaceColor',[0,0,1]);hold on;
     axis equal;
 end
-title('Pristine Lattice');
+title('ZCNT');
+
 %Plot borders of unit cells
 line([(Unit_L - 0.5*d1) (Unit_L - 0.5*d1)],[-1.5 (D_w+1.5)],'Color','k','LineWidth',1);
 line([(2*Unit_L - 0.5*d1) (2*Unit_L - 0.5*d1)],[-1.5 (D_w+1.5)],'Color','k','LineWidth',1);
 
 fprintf('\t\t\t-> Done!\n\n');
 fprintf('Making Hamiltonians...');
-
 N_Unit = n*N_Ring_Unit; % The number of atoms in each unit cell
 
 atom_x0 = zeros(1,N_Unit);
@@ -82,11 +84,11 @@ atom_yl = zeros(1,N_Unit);
 atom_xr = zeros(1,N_Unit);
 atom_yr = zeros(1,N_Unit);
 
+% Hamiltonian matrices
 H0=zeros(N_Unit,N_Unit);
 HL=zeros(N_Unit,N_Unit);
 HR=zeros(N_Unit,N_Unit);
 
-% Fill left, middle and right unit cells
 for i=1:N_Unit
     atom_xl(i)=atom_x(i);
     atom_yl(i)=atom_y(i);
@@ -102,12 +104,16 @@ for i=1:N_Unit
     atom_yr(i)=atom_y(i+2*N_Unit);
 end
 
-%H0
+Rows_Interval = d1*sind(60);
+Roll_Over = D_w + Rows_Interval;
+
+% Generate H0
 for j=1:N_Unit
     for i=1:N_Unit
         
         dis = sqrt( (atom_x0(j)-atom_x0(i))^2 + (atom_y0(j)-atom_y0(i))^2 );
         
+        %We compare the distances and assign a hopping parameter
         if  dis < mrg
             H0(j,i) = eps;
             
@@ -126,7 +132,34 @@ for j=1:N_Unit
     end
 end
 
-%HL
+for j=1:N_Unit
+    
+    % Roll over atoms at the edges of AGNR to build Hamiltonian of ZCNT
+    if ( (atom_y0(j) == 0) || (atom_y0(j) == Rows_Interval) )
+        
+        for i=1:N_Unit
+            
+            dis = sqrt( (atom_x0(j)-atom_x0(i))^2 + ((atom_y0(j)+Roll_Over)-atom_y0(i))^2 );
+            
+            if (d1-mrg<dis) && (dis<d1+mrg)
+                H0(j,i) = t1;
+                H0(i,j) = t1;
+                
+            elseif (d2-mrg<dis) && (dis<d2+mrg)
+                H0(j,i) = t2;
+                H0(i,j) = t2;
+                
+            elseif (d3-mrg<dis) && (dis<d3+mrg)
+                H0(j,i) = t3;
+                H0(i,j) = t3;
+            end
+        end
+    end
+end
+
+
+
+% Generate HL
 for j=1:N_Unit
     for i=1:N_Unit
         
@@ -150,7 +183,33 @@ for j=1:N_Unit
     end
 end
 
-%HR
+for j=1:N_Unit
+    
+    % Roll over atoms at the edges of AGNR to biuld Hamiltonian of ZCNT
+    if ( (atom_y0(j) == 0) || (atom_y0(j) == Rows_Interval) )
+        
+        for i=1:N_Unit
+            
+            dis = sqrt( (atom_x0(j)-atom_xl(i))^2 + ((atom_y0(j)+Roll_Over)-atom_yl(i))^2 );
+            
+            if (d1-mrg<dis) && (dis<d1+mrg)
+                HL(j,i) = t1;
+                HL(i,j) = t1;
+                
+            elseif (d2-mrg<dis) && (dis<d2+mrg)
+                HL(j,i) = t2;
+                HL(i,j) = t2;
+                
+            elseif (d3-mrg<dis) && (dis<d3+mrg)
+                HL(j,i) = t3;
+                HL(i,j) = t3;
+            end
+        end
+    end
+end
+
+
+% Generate HR
 for j=1:N_Unit
     for i=1:N_Unit
         
@@ -174,14 +233,40 @@ for j=1:N_Unit
     end
 end
 
-fprintf('\t\t\t> Done!\n\n');
+for j=1:N_Unit
+    
+    % Roll over atoms at the edges of AGNR to biuld Hamiltonian of ZCNT
+    if ( (atom_y0(j) == 0) || (atom_y0(j) == Rows_Interval) )
+        
+        for i=1:N_Unit
+            
+            dis = sqrt( (atom_x0(j)-atom_xr(i))^2 + ((atom_y0(j)+Roll_Over)-atom_yr(i))^2 );
+            
+            if (d1-mrg<dis) && (dis<d1+mrg)
+                HR(j,i) = t1;
+                HR(i,j) = t1;
+                
+            elseif (d2-mrg<dis) && (dis<d2+mrg)
+                HR(j,i) = t2;
+                HR(i,j) = t2;
+                
+            elseif (d3-mrg<dis) && (dis<d3+mrg)
+                HR(j,i) = t3;
+                HR(i,j) = t3;
+            end
+        end
+    end
+end
+fprintf('\t\t\t-> Done!\n\n');
+
 disp=[];
 p=1;
 for ka=-pi:2*pi/500:pi
-    HK=H0+HR*exp(1i*ka)+HL*exp(-1i*ka); %Combine matrices to make Hamiltonian
+    HK=H0+HR*exp(1i*ka)+HL*exp(-1i*ka);
     ee=eig(HK);
     disp=[disp,ee];
     
+    % Extract bandgap value
     for j=1:length(ee)
         if ee(j)>eps
             g(p)=ee(j);
@@ -199,9 +284,8 @@ plot(-pi:2*pi/500:pi,disp);
 title('Band Structure');
 xlabel('Ka');
 ylabel('Energy(ev)');
-string = sprintf('BG = %.2f eV, %d-AGNR',GAP,n);
+string = sprintf('BG = %.2f eV, %d-ZCNT',GAP,n/2);
 legend(string);
-
-fprintf('\t\t> done, Pristine Band Gap = %.2f eV\n\n',GAP);
-fprintf('\n\n> Done!\n\n');
-fprintf('\n\n\nSimulation Is Complete');
+fprintf('\t\t-> done, Pristine Band Gap = %.2f eV\n\n',GAP);
+fprintf('\n\n-> Done!\n\n');
+fprintf('\n\n\n\t\t\tSimulation Is Complete');
